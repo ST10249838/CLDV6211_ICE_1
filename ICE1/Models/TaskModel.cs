bc666ba0df53aac64b25a326f1a44ddd793eae58
Blaise Mikka de Gier <ST10249838@vcconnect.edu.za>
2024-03-19 23:54:27 +0200

    Validation

--- ICE1/Models/TaskModel.cs
+++ ICE1/Models/TaskModel.cs
@@ -6,11 +6,15 @@ namespace ICE1Project.Models
     {
         [Key]
         public int Id { get; set; }
+        [StringLength(60, MinimumLength = 2)]
         [Required]
         public string? Title { get; set; }
+        [StringLength(200, MinimumLength = 2)]
         public string? Description { get; set; }
+        [Display(Name = "Start Date")]
         [DataType(DataType.Date)]
         public DateTime StartTask { get; set; }
+        [Display(Name = "End Date")]
         [DataType(DataType.Date)]
         public DateTime EndTask { get; set; }
     }
--- ICE1/obj/Debug/net8.0/ICE1.AssemblyInfo.cs
+++ ICE1/obj/Debug/net8.0/ICE1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ICE1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0767ac7c9f618f3cfea5be597d3941e8afa497a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf26f6caba601562a2922b7aead989c14ae6a23")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICE1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICE1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
